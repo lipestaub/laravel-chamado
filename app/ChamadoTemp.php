278f25b92f83ae411dd874c7037719fc912b4ad4
Felipe Staub <lipestaub@gmail.com>
2023-01-19 13:50:36 -0300

    feat: preencher fillable do model

--- app/ChamadoTemp.php
+++ app/ChamadoTemp.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class ChamadoTemp extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'nome',
+        'empresa',
+        'email',
+        'telefone',
+        'titulo',
+        'mensagem',
+        'anexo',
+        'chave_acesso',
+        'datahora',
+        'ip',
+    ];
 }
