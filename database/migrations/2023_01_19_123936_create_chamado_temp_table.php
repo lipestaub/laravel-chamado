77fc80d5a8cd41f0f064813e0a0376edc26298b5
Felipe Staub <lipestaub@gmail.com>
2023-01-19 10:06:31 -0300

    feat: criado migration, model e controller

---
+++ database/migrations/2023_01_19_123936_create_chamado_temp_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChamadoTempsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chamado_temp', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nome');
+            $table->string('empresa');
+            $table->string('email');
+            $table->string('telefone');
+            $table->string('titulo');
+            $table->text('mensagem');
+            $table->string('anexo')->nullable();
+            $table->string('chave_acesso');
+            $table->integer('datahora');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('chamado_temp');
+    }
+}
