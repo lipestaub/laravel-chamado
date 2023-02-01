779cc28847eb72d1a6af5204c86c7f9176db454c
Felipe Staub <lipestaub@gmail.com>
2023-02-01 08:21:36 -0300

    feat: criado layout email

---
+++ resources/views/emails/chamadoAberto.blade.php
@@ -0,0 +1,5 @@
+<b>Ol&aacute;, {{ $data['nome'] }} </b>
+<br>
+<span>Seu chamado foi aberto com sucesso!</span>
+<br>
+<span>Para acompanhar o andamento <a href="localhost:4200/chamados/status/{{ $data['chave_acesso'] }}">clique aqui</a></span>
